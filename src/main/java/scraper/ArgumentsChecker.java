135f5dd7f8111acf0ee2339faeb790187abd5230
katooshka <ek.zabara@yandex.ru>
2017-02-08 01:52:01 +0000

    Add ArgumentsChecker class and corresponding tests

---
+++ src/main/java/scraper/ArgumentsChecker.java
@@ -0,0 +1,43 @@
+package scraper;
+
+public class ArgumentsChecker {
+    private static final String FUNCTION_NAME = "--posts";
+    private static final int MAX_POSTS_NUMBER = 100;
+
+    /**
+     * Returns number of posts that are to be printed.
+     * The first argument represents the name of the function that outputs top Hacker News posts
+     * and should be equal to '--posts'. The second argument represents a number of posts to be printed
+     * and should be a value from 1 to 100 inclusive.
+     *
+     * @param args a String array containing name of the print function and number of posts to be printed
+     * @return number of posts to be printed
+     * @throws IllegalArgumentException if number of arguments differs from two
+     *                                  or if the format of arguments is wrong
+     */
+    public static int readArguments(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Wrong number of arguments");
+        }
+        String firstArgument = args[0];
+        if (!firstArgument.equals(FUNCTION_NAME)) {
+            throw new IllegalArgumentException("Wrong first argument");
+        }
+        int secondArgument;
+        try {
+            secondArgument = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Second argument is not a number");
+        }
+        if (secondArgument < 0) {
+            throw new IllegalArgumentException("Second argument is a negative number");
+        }
+        if (secondArgument == 0) {
+            throw new IllegalArgumentException("Second argument is zero");
+        }
+        if (secondArgument > MAX_POSTS_NUMBER) {
+            throw new IllegalArgumentException("Second argument is greater than " + MAX_POSTS_NUMBER);
+        }
+        return secondArgument;
+    }
+}
---
+++ src/test/java/scraper_test/ArgumentCheckerTest.java
@@ -0,0 +1,70 @@
+package scraper_test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static scraper.ArgumentsChecker.readArguments;
+
+public class ArgumentCheckerTest {
+
+    @Test
+    public void readArguments_shouldThrow_whenWrongArgumentsNumber() {
+        try {
+            readArguments(new String[]{"a", "b", "c"});
+            fail("IllegalArgumentException expected");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Wrong number of arguments", e.getMessage());
+        }
+    }
+
+    @Test
+    public void readArguments_shouldThrow_whenWrongFirstArgument() {
+        try {
+            readArguments(new String[]{"abc", "90"});
+            fail("IllegalArgumentException expected");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Wrong first argument", e.getMessage());
+        }
+    }
+
+    @Test
+    public void readArguments_shouldThrow_whenSecondArgumentNotNumber() {
+        try {
+            readArguments(new String[]{"--posts", "abc"});
+            fail("IllegalArgumentException expected");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Second argument is not a number", e.getMessage());
+        }
+    }
+
+    @Test
+    public void readArguments_shouldThrow_whenSecondArgumentIsNegative() {
+        try {
+            readArguments(new String[]{"--posts", "-1"});
+            fail("IllegalArgumentException expected");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Second argument is a negative number", e.getMessage());
+        }
+    }
+
+    @Test
+    public void readArguments_shouldThrow_whenSecondArgumentIsZero() {
+        try {
+            readArguments(new String[]{"--posts", "0"});
+            fail("IllegalArgumentException expected");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Second argument is zero", e.getMessage());
+        }
+    }
+
+    @Test
+    public void readArguments_shouldThrow_whenSecondArgumentIsGreaterThanMaxPostsNumber() {
+        try {
+            readArguments(new String[]{"--posts", "101"});
+            fail("IllegalArgumentException expected");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Second argument is greater than 100", e.getMessage());
+        }
+    }
+}
